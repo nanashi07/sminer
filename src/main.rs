fa425043c20b274c07d0f6757a00111f68e5630d
Bruce Tsai <bruce.tsai@opennet.tw>
2022-03-31 15:39:56 +0000

    add clear annotation command

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use chrono::{Duration, Utc};
+use chrono::{DateTime, Duration, TimeZone, Utc};
 use clap::{Arg, ArgMatches, Command};
 use log::{debug, info};
 use sminer::{
@@ -8,6 +8,7 @@ use sminer::{
         es::{
             index_protfolios_from_file, index_tickers_from_file, take_index_time, ticker_index_name,
         },
+        grafana::clear_annotations,
         mongo::{export, import},
     },
     provider::yahoo::consume,
@@ -34,114 +35,22 @@ async fn main() -> Result<()> {
 
             match name {
                 "consume" => {
-                    // add additional config
-                    config.extra_put(KEY_EXTRA_PRCOESS_IN_ASYNC, "async_mode");
-                    let context = AppContext::new(config).init().await?;
-                    let config = context.config();
-
-                    let symbols = config.symbols();
-                    let units = config.time_units();
-                    let uri = config.platform.yahoo.uri.as_str();
-
-                    info!("Loaded symbols: {:?}", &symbols);
-                    info!(
-                        "Loaded time units: {:?}",
-                        &units.iter().map(|u| u.name.clone()).collect::<Vec<_>>()
-                    );
-                    consume(&context, &uri, &symbols, Option::None).await?;
+                    perform_consume(&mut config, sub_matches).await?;
                 }
                 "replay" => {
-                    let start_time = Utc::now().timestamp_millis();
-                    config_truncat(&mut config, sub_matches)?;
-                    let context = AppContext::new(config).init().await?;
-                    let config = context.config();
-
-                    info!(
-                        "Available time unit: {:?}",
-                        config
-                            .time_units()
-                            .iter()
-                            .map(|u| u.name.clone())
-                            .collect::<Vec<_>>()
-                    );
-
-                    let files: Vec<&str> = sub_matches.values_of("files").unwrap().collect();
-                    debug!("Input files: {:?}", files);
-
-                    for file in files {
-                        // TODO: try renew context for release resource
-                        // compare to original if speed up
-                        replay(&context, &file, ReplayMode::Sync).await?
-                    }
-
-                    info!(
-                        "Replay time cost: {}",
-                        Duration::milliseconds(Utc::now().timestamp_millis() - start_time)
-                    )
+                    perform_replay(&mut config, sub_matches).await?;
                 }
                 "import" => {
-                    config_truncat(&mut config, sub_matches)?;
-
-                    let context = AppContext::new(config).init().await?;
-
-                    let files: Vec<&str> = sub_matches.values_of("files").unwrap().collect();
-                    debug!("Input files: {:?}", files);
-                    for file in files {
-                        import(&context, &file).await?;
-                    }
+                    perform_import(&mut config, sub_matches).await?;
                 }
                 "export" => {
-                    let context = AppContext::new(config).init().await?;
-
-                    let files: Vec<&str> = sub_matches.values_of("collections").unwrap().collect();
-                    debug!("Target collections: {:?}", files);
-                    for file in files {
-                        export(&context, &file).await?;
-                    }
+                    perform_export(&mut config, sub_matches).await?;
                 }
                 "index" => {
-                    config_truncat(&mut config, sub_matches)?;
-
-                    let context = AppContext::new(config).init().await?;
-                    let config = context.config();
-                    let persistence = context.persistence();
-
-                    let r#type = sub_matches.value_of("type").unwrap().to_lowercase();
-                    debug!("Input type: {}", &r#type);
-                    let files: Vec<&str> = sub_matches.values_of("files").unwrap().collect();
-                    debug!("Input files: {:?}", files);
-
-                    let mut drop_history: HashSet<String> = HashSet::new();
-
-                    match r#type.as_str() {
-                        "ticker" => {
-                            for file in files {
-                                // drop index when fit name at first time
-                                let index_time = take_index_time(file);
-                                let index_name = ticker_index_name(&index_time);
-                                if config.truncat_enabled() && !drop_history.contains(&index_name) {
-                                    persistence.delete_index(&index_name).await?;
-                                    drop_history.insert(index_name);
-                                }
-
-                                index_tickers_from_file(&context, file).await?;
-                            }
-                        }
-                        "protfolio" => {
-                            for file in files {
-                                // drop index when fit name at first time
-                                let index_time = take_index_time(file);
-                                let index_name = ticker_index_name(&index_time);
-                                if config.truncat_enabled() && !drop_history.contains(&index_name) {
-                                    persistence.delete_index(&index_name).await?;
-                                    drop_history.insert(index_name);
-                                }
-
-                                index_protfolios_from_file(&context, file).await?;
-                            }
-                        }
-                        _ => {}
-                    }
+                    perform_index(&mut config, sub_matches).await?;
+                }
+                "annotate" => {
+                    perform_annotate(&mut config, sub_matches).await?;
                 }
                 _ => {}
             }
@@ -151,6 +60,169 @@ async fn main() -> Result<()> {
             println!();
         }
     }
+
+    Ok(())
+}
+
+async fn perform_consume(config: &mut AppConfig, _sub_matches: &ArgMatches) -> Result<()> {
+    // add additional config
+    config.extra_put(KEY_EXTRA_PRCOESS_IN_ASYNC, "async_mode");
+    let context = AppContext::new(config.to_owned()).init().await?;
+    let config = context.config();
+
+    let symbols = config.symbols();
+    let units = config.time_units();
+    let uri = config.platform.yahoo.uri.as_str();
+
+    info!("Loaded symbols: {:?}", &symbols);
+    info!(
+        "Loaded time units: {:?}",
+        &units.iter().map(|u| u.name.clone()).collect::<Vec<_>>()
+    );
+    consume(&context, &uri, &symbols, Option::None).await?;
+
+    Ok(())
+}
+
+async fn perform_replay(config: &mut AppConfig, sub_matches: &ArgMatches) -> Result<()> {
+    let start_time = Utc::now().timestamp_millis();
+    config_truncat(config, sub_matches)?;
+    let context = AppContext::new(config.to_owned()).init().await?;
+    let config = context.config();
+
+    info!(
+        "Available time unit: {:?}",
+        config
+            .time_units()
+            .iter()
+            .map(|u| u.name.clone())
+            .collect::<Vec<_>>()
+    );
+
+    let files: Vec<&str> = sub_matches.values_of("files").unwrap().collect();
+    debug!("Input files: {:?}", files);
+
+    for file in files {
+        // TODO: try renew context for release resource
+        // compare to original if speed up
+        replay(&context, &file, ReplayMode::Sync).await?
+    }
+
+    info!(
+        "Replay time cost: {}",
+        Duration::milliseconds(Utc::now().timestamp_millis() - start_time)
+    );
+
+    Ok(())
+}
+
+async fn perform_import(config: &mut AppConfig, sub_matches: &ArgMatches) -> Result<()> {
+    config_truncat(config, sub_matches)?;
+
+    let context = AppContext::new(config.to_owned()).init().await?;
+
+    let files: Vec<&str> = sub_matches.values_of("files").unwrap().collect();
+    debug!("Input files: {:?}", files);
+    for file in files {
+        import(&context, &file).await?;
+    }
+
+    Ok(())
+}
+
+async fn perform_export(config: &mut AppConfig, sub_matches: &ArgMatches) -> Result<()> {
+    let context = AppContext::new(config.to_owned()).init().await?;
+
+    let files: Vec<&str> = sub_matches.values_of("collections").unwrap().collect();
+    debug!("Target collections: {:?}", files);
+    for file in files {
+        export(&context, &file).await?;
+    }
+
+    Ok(())
+}
+
+async fn perform_index(config: &mut AppConfig, sub_matches: &ArgMatches) -> Result<()> {
+    config_truncat(config, sub_matches)?;
+
+    let context = AppContext::new(config.to_owned()).init().await?;
+    let config = context.config();
+    let persistence = context.persistence();
+
+    let r#type = sub_matches.value_of("type").unwrap().to_lowercase();
+    debug!("Input type: {}", &r#type);
+    let files: Vec<&str> = sub_matches.values_of("files").unwrap().collect();
+    debug!("Input files: {:?}", files);
+
+    let mut drop_history: HashSet<String> = HashSet::new();
+
+    match r#type.as_str() {
+        "ticker" => {
+            for file in files {
+                // drop index when fit name at first time
+                let index_time = take_index_time(file);
+                let index_name = ticker_index_name(&index_time);
+                if config.truncat_enabled() && !drop_history.contains(&index_name) {
+                    persistence.delete_index(&index_name).await?;
+                    drop_history.insert(index_name);
+                }
+
+                index_tickers_from_file(&context, file).await?;
+            }
+        }
+        "protfolio" => {
+            for file in files {
+                // drop index when fit name at first time
+                let index_time = take_index_time(file);
+                let index_name = ticker_index_name(&index_time);
+                if config.truncat_enabled() && !drop_history.contains(&index_name) {
+                    persistence.delete_index(&index_name).await?;
+                    drop_history.insert(index_name);
+                }
+
+                index_protfolios_from_file(&context, file).await?;
+            }
+        }
+        _ => {}
+    }
+
+    Ok(())
+}
+
+async fn perform_annotate(_config: &mut AppConfig, sub_matches: &ArgMatches) -> Result<()> {
+    let mut from: Option<DateTime<Utc>> = None;
+    let mut to: Option<DateTime<Utc>> = None;
+
+    if let Some(start) = sub_matches.value_of("start") {
+        if let Ok(time) = DateTime::parse_from_rfc3339(start) {
+            from = Some(time.with_timezone(&Utc));
+        }
+        if let Ok(timestamp) = start.parse::<i64>() {
+            from = Some(Utc.timestamp_millis(timestamp));
+        }
+    }
+
+    if let Some(end) = sub_matches.value_of("end") {
+        if let Ok(time) = DateTime::parse_from_rfc3339(end) {
+            to = Some(time.with_timezone(&Utc));
+        }
+        if let Ok(timestamp) = end.parse::<i64>() {
+            to = Some(Utc.timestamp_millis(timestamp));
+        }
+    }
+
+    let tags: Vec<String> = sub_matches
+        .values_of("tag")
+        .unwrap_or_default()
+        .map(|s| s.to_owned())
+        .collect();
+
+    info!(
+        "Clear annotations, start: {:?}, end: {:?}, tags: {:?}",
+        from, to, &tags
+    );
+
+    clear_annotations(from, to, &tags).await?;
 
     Ok(())
 }
@@ -263,6 +335,30 @@ fn command_args<'help>() -> Command<'help> {
                         .multiple_values(true)
                         .required(true)
                         .help("Source files to be indexed"),
+                ]),
+            Command::new("annotate")
+                .about("Remove annotations from grafana")
+                .args(&[
+                    level.clone(),
+                    config_file.clone(),
+                    Arg::new("start")
+                        .short('s')
+                        .long("start")
+                        .takes_value(true)
+                        .required(false)
+                        .help("Start time"),
+                    Arg::new("end")
+                        .short('e')
+                        .long("end")
+                        .takes_value(true)
+                        .required(false)
+                        .help("End time"),
+                    Arg::new("tag")
+                        .short('t')
+                        .long("tag")
+                        .takes_value(true)
+                        .required(false)
+                        .multiple_occurrences(true),
                 ]),
         ])
 }
